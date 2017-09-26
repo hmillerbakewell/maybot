96303e8829ddbc1afcfdbbbfd45de98dd63489ab
Maryam <maryam.oxford@gmail.com>
2017-09-26 15:34:29 +0100

    Added char-rnn-tensorflow and word-rnn-tensorflow, concatenated training data

--- dataclean.py
+++ dataclean.py
@@ -4,16 +4,13 @@
 import os
 import re
 
-# Load and clean all txt files
-for filename in os.listdir("./data"):
-    if filename.endswith(".txt"):
-        file = open("./data/"+filename,"r")
-        filetxt_raw = file.read()
-        file.close()
-        filetxt_cl1 = re.sub('[\-]+', " ", filetxt_raw) # replace hyphens with spaces
-        filetxt_cl2 = re.sub('[^A-Za-z0-9\.\,\'" "]+', "", filetxt_cl1) # remove non alphanumeric characters
-        filetxt_cl3 = re.sub(r'\.([a-zA-Z])', r'. \1', filetxt_cl2) # insert spaces after full stops where absent
-        savename = filename[0:-4]+"_CLEAN.txt" # save as a CSV
-        file = open("./data/"+savename,"w") # create a new file to write the clean data to
-        file.write(filetxt_cl3)
+catfile = ""
 
+# Load and clean all txt files
+for filename in os.listdir("./data/Final Mix"):
+    if filename.endswith(".txt"):
+        file = open("./data/Final Mix/"+filename,"r")
+        catfile = catfile+ file.read()
+        file.close()
+file = open("FinalMix.txt","w") # create a new file to write the clean data to
+file.write(catfile)
